51b05eb359588a917a9f0699b0437c500637fc6f
YashSinghGa <Yashgahlotsingh@gmail.com>
2026-06-06 20:54:05 +0530

    Add note by id route and 404 handling

--- 01-notes-api/app.js
+++ 01-notes-api/app.js
@@ -10,10 +10,37 @@ app.get("/", (req, res) => {
 app.get("/notes", (req, res) => {
   const notes = JSON.parse(fs.readFileSync("./data/notes.json", "utf-8"));
 
-  res.json({
+  res.status(200).json({
     status: "success",
     results: notes.length,
     data: notes,
+  });
+});
+
+app.get("/notes/:id", (req, res) => {
+  const notes = JSON.parse(fs.readFileSync("./data/notes.json", "utf-8"));
+
+  const id = Number(req.params.id);
+
+  const note = notes.find((el) => el.id === id);
+
+  if (!note) {
+    return res.status(404).json({
+      status: "fail",
+      message: "Note not found",
+    });
+  }
+
+  res.status(200).json({
+    status: "success",
+    data: note,
+  });
+});
+
+app.all("*", (req, res) => {
+  res.status(404).json({
+    status: "fail",
+    message: `Can't find ${req.originalUrl} on this server`,
   });
 });
 
